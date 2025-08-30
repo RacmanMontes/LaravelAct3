0b4bb6fc34b87a33ec97e44648a567daf906fb24
lhenard Sumbad <sumbadlhenard1@gmail.com>
2025-08-30 20:14:06 +0800

    added mine

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
+
 
 Route::get('/home', function () {
     return view('home');
 })->name('home');
+
 Route::get('/home/montes', function () {
     return view('montes');
 })->name('montes');
+
 Route::get('/cv', function () {
     return view('cv');
 })->name('cv');
+Route::get('/sumbad', function () {
+    return view('sumbad');
+})->name('sumbad');
